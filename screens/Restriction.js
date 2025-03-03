b9c42ddd8583a2e1f1a4f44ad866f011cdb3e869
Sergio Capsule <sergiorobin1307@gmail.com>
2025-03-03 14:11:10 +0100

    Added buttons to restrictions page

--- screens/Restriction.js
+++ screens/Restriction.js
@@ -1,18 +1,66 @@
-import { TextInput, TouchableOpacity, View, Text, StyleSheet, Link } from "react-native";
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 export default function Restriction({ navigation }) {
-    return (
-        <View>
-            <Text>Restrictionpage</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome Name, what are your dietary preferences?
+      </Text>
+      <View style={styles.allButtons}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Muscle gain</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Healthy</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Gluten free</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Pregnant</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Vegetarian</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "blue",
+    borderStyle: "solid",
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  allButtons: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+    width: "80%",
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    padding: 10,
+    width: "100%",
+    backgroundColor: "#33652C",
+    borderRadius: 5,
+    margin: 8,
+  },
+  text: {
+    color: "white",
+    fontFamily: "Inter",
+  },
+});
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,29 +1,35 @@
-import { TextInput, TouchableOpacity, View, Text, StyleSheet, Link } from "react-native";
+import {
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
 
 export default function Signup() {
-    return (
-        <View>
-            <Text>MyFood</Text>
-            <Text>Connection</Text>
-            <Text>Set your email and password</Text>
-            <TextInput placeholder="email"></TextInput>
-            <TextInput placeholder="password"></TextInput>
-            <TouchableOpacity activeOpacity={0.8} style={styles.button}>
-            <Text style={styles.textButton}>Connection</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <Text>Create an account</Text>
-            <Text>Continue without account</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  return (
+    <View>
+      <Text>MyFood</Text>
+      <Text>Connection</Text>
+      <Text>Set your email and password</Text>
+      <TextInput placeholder="email"></TextInput>
+      <TextInput placeholder="password"></TextInput>
+      <TouchableOpacity activeOpacity={0.8} style={styles.button}>
+        <Text style={styles.textButton}>Connection</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Create an account</Text>
+        <Text>Continue without account</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
