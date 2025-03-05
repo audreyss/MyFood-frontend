aed724df34514d8ef33ad909779c40f8416690cc
Audrey Seignol <aseignol@yahoo.fr>
2025-03-05 15:32:15 +0100

    add diet into reducer

--- reducers/user.js
+++ reducers/user.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    value: { username: null, token: null },
+    value: { username: null, token: null, diet: null },
   }; 
 
 export const userSlice = createSlice({
@@ -11,13 +11,18 @@ export const userSlice = createSlice({
         login: (state, action) => {
             state.value.username = action.payload.username;
             state.value.token = action.payload.token;
+            state.value.diet = null;
         },
         logout: (state) => {
             state.value.username = null;
             state.value.token = null;
+            state.value.diet = null;
+        },
+        addDiet: (state, action) => {
+            state.value.diet = action.payload
         }
     }
 })
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, addDiet } = userSlice.actions;
 export default userSlice.reducer;
